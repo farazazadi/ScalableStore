0e2575e77c5ea80e2012dc21b53df6584df8cffc
Faraz Azadi <farazazadi70@gmail.com>
2022-10-10 20:37:41 +0300

    Add Domain base classes

---
+++ Source/DemoStore.Services.CommandSide.Domain/Common/DomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoStore.Services.CommandSide.Domain.Common;
+
+public abstract class DomainEvent
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public DateTimeOffset OccurredOn { get; } = DateTimeOffset.Now;
+
+}
